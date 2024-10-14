bf5ca3d663649efc2dab2ccf37e9e0890447121e
BrawnyMan <75688115+BrawnyMan@users.noreply.github.com>
2024-10-14 21:23:55 +0200

    Create credentials.h

---
+++ credentials.h
@@ -0,0 +1,9 @@
+// credentials.h
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Wi-Fi Credentials
+const char* WIFI_SSID = "Your_SSID";
+const char* WIFI_PASSWORD = "Your_PASSWORD";
+
+#endif // CREDENTIALS_H
